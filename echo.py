7bb9fab46d7ce175aa0646b200cccf80165516af
DarekKarasiewicz <dar.karasiewicz@gmail.com>
2021-05-07 15:07:27 +0200

    Working echo

--- echo.py
+++ echo.py
@@ -1,1 +1,25 @@
 import sys
+
+
+terminator="\n"
+reverse=False
+sparator= " "
+for i,each in enumerate(sys.argv[1:]):
+    if each =="-n":
+        terminator = ""
+    elif each=="-r":
+        reverse=True
+    elif each=="-l":
+        sparator="\n"
+    else:
+        break
+
+args=sys.argv[i+1:]
+if reverse:
+    args=reversed(args)
+
+print(sparator.join(args),end=terminator)
+
+# lista=["x","y","z"]
+# print(lista)
+# print(list(enumerate(lista)))
